91e7663adf5c628f3d3b007f31faae34b5d2b412
annekeller <annekeller93@gmail.com>
2017-04-05 20:22:39 +0200

    added singleton pattern in User class

--- foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/model/User.java
+++ foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/model/User.java
@@ -5,6 +5,19 @@ package corp.skaj.foretagskvitton.model;
  */
 
 public class User extends Employee {
+    private static User user = new User();
 
+    private User () {
+       //TODO
+        //Here we have to collect saved data, when app is closed and we reopen... We always want the same user.
+    }
+
+    /**
+     * This method returns the user
+    * @return the only user
+     */
+    public static User getUser() {
+        return user;
+    }
 
 }
