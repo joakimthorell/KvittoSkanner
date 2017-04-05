726f15b8dbf6ce019abea7f783847b094ee8d091
mattssonj <joamatt@student.chalmers.se>
2017-04-05 16:47:37 +0200

    added packages

--- foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/controllers/MainActivity.java
+++ foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/controllers/MainActivity.java
@@ -1,7 +1,9 @@
-package corp.skaj.foretagskvitton;
+package corp.skaj.foretagskvitton.controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import corp.skaj.foretagskvitton.R;
 
 public class MainActivity extends AppCompatActivity {
 
