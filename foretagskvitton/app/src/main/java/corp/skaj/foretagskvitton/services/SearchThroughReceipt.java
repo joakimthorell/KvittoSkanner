90b3dfd7855bd5e03a607cdf488807baa9eb1956
annekeller <annekeller93@gmail.com>
2017-04-27 15:18:11 +0200

    methods added to service package

--- foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/services/SearchThroughReceipt.java
+++ foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/services/SearchThroughReceipt.java
@@ -1,5 +1,6 @@
 package corp.skaj.foretagskvitton.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -12,10 +13,12 @@ public class SearchThroughReceipt {
 
     double totalCost;
     List<String> list;
+    List<Double> listOfDoubles = new ArrayList<>();
 
 
     public void findTotalCost() {
-
+    findAllDoubles();
+    totalCost = findBiggestDouble();
     }
 
     public void findCardNumber () {
@@ -30,4 +33,23 @@ public class SearchThroughReceipt {
 
     }
 
+    public void findAllDoubles () {
+        double temp = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).contains(",")) {
+                temp = Double.parseDouble(list.get(i));
+                listOfDoubles.add(temp);
+            }
+        }
+    }
+
+    public double findBiggestDouble () {
+        double biggestDouble = 0;
+        for (int i = 0; i < listOfDoubles.size(); i++) {
+            if (listOfDoubles.get(i) > listOfDoubles.get(i+1)) {
+                biggestDouble = listOfDoubles.get(i);
+            }
+        }
+        return biggestDouble;
+    }
 }
