49efe94f6fab89626f663e1deb887b60ec9cd14f
KevinBrunstrom <Kevinbr@student.chalmers.se>
2017-04-27 15:20:06 +0200

    added a beta method for finding dates, needs testing

--- foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/services/SearchThroughReceipt.java
+++ foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/services/SearchThroughReceipt.java
@@ -1,7 +1,9 @@
 package corp.skaj.foretagskvitton.services;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
-
 
 /**
  * Created by annekeller on 2017-04-27.
@@ -12,8 +14,8 @@ public class SearchThroughReceipt {
 
     double totalCost;
     List<String> list;
-
-
+    DateFormat date;
+    Calendar calandar;
     public void findTotalCost() {
 
     }
@@ -22,8 +24,23 @@ public class SearchThroughReceipt {
 
     }
 
-    public void findDate ( ) {
+    public String findDate ( ) {
+        for(int i = 0; i < list.size(); i++){
+            if(correctFirstnum(list.get(i).substring(0,3)) && correctLength(list.get(i))){
+                return list.get(i);
+            }
+        }
+        return calandar.getInstance().getTime().toString();
+    }
 
+    // Checks that the string starts with the current year in ex. 17 or 2017.
+    private boolean correctFirstnum(String date){
+        return  date.substring(0,2).equals((String.valueOf(calandar.YEAR).substring(1,3))) ||
+                date.equals(String.valueOf(calandar.YEAR));
+    }
+    // Checks that the size is correct format, either 170218 or 2017-05-03.
+    private boolean correctLength(String date){
+        return date.length() <= 10 || date.length() >= 6;
     }
 
     public void findProducts () {
