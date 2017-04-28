eeabe65627c97fe9b14e33f55df6377a93670ea6
lasanjin <slavnic@student.chalmers.se>
2017-04-28 15:42:06 +0200

    working on activities

--- foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/controllers/InitApplication.java
+++ foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/controllers/InitApplication.java
@@ -1,10 +1,14 @@
 package corp.skaj.foretagskvitton.controllers;
 
+import corp.skaj.foretagskvitton.model.User;
+
 public class InitApplication {
     private static InitApplication instance;
+    private User user;
 
     private InitApplication() {
         // TODO Load from txt file
+        user = new User("User"); //Tillfällig lösning
     }
 
     public static InitApplication getInstance() {
