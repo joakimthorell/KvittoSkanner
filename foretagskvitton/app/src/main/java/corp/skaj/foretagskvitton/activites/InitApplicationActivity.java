4cf98e708760427c08ca6d5dc3a1d2cc54245797
mattssonj <joamatt@student.chalmers.se>
2017-04-28 14:32:48 +0200

    added init application activity that will load when app starts

---
+++ foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/activites/InitApplicationActivity.java
@@ -0,0 +1,15 @@
+package corp.skaj.foretagskvitton.activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import corp.skaj.foretagskvitton.R;
+
+public class InitApplicationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init_application);
+    }
+}
