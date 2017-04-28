0beb90f0efe1798fa0157d341e08fc3c0f64444d
mattssonj <joamatt@student.chalmers.se>
2017-04-28 15:08:13 +0200

    started the init class

--- foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/controllers/InitApplication.java
+++ foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/controllers/InitApplication.java
@@ -1,4 +1,17 @@
 package corp.skaj.foretagskvitton.controllers;
 
 public class InitApplication {
+
+    private static InitApplication instance;
+
+    private InitApplication() {
+        // TODO Load from txt file
+    }
+
+    public static InitApplication getInstance() {
+        if (instance == null) {
+            instance = new InitApplication();
+        }
+        return instance;
+    }
 }
