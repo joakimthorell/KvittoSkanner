cea4b1adfc23d8f3ae87f2681904255a95031204
KevinBrunstrom <Kevinbr@student.chalmers.se>
2017-05-04 15:02:03 +0200

    Tried to add a picture holder in the list view. Not able to try the result beacuse of bugs..

--- foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/activities/ReceiptArchiveActivity.java
+++ foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/activities/ReceiptArchiveActivity.java
@@ -1,9 +1,62 @@
 package corp.skaj.foretagskvitton.activities;
 
-/**
- * Created by kevinbrunstrom on 2017-05-03.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class ReceiptArchiveActivity {
+import com.roughike.bottombar.BottomBar;
 
+import corp.skaj.foretagskvitton.controllers.BottomNavigationController;
+import corp.skaj.foretagskvitton.view.CustomList;
+
+public class ReceiptArchiveActivity extends AppCompatActivity {
+    ListView list;
+
+    public static final String STATE_FOR_BOTTOM_MENY = "corp.skaj.foretagskvitton.ARCHIVE";
+
+        // Array of strings...
+        String[] web = {"Android","IPhone","WindowsMobile","Blackberry",
+                "WebOS","Ubuntu","Windows7","Max OS X"};
+
+    Integer[] imageId = {
+            R.drawable.ic_camera,
+            R.drawable.ic_camera,
+            R.drawable.ic_camera,
+            R.drawable.ic_camera,
+            R.drawable.ic_camera,
+            R.drawable.ic_camera,
+            R.drawable.ic_camera,
+            R.drawable.ic_camera,
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CustomList adapter = new
+                CustomList(ReceiptArchiveActivity.this, web, imageId);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ReceiptArchiveActivity.this, "You Clicked at " + web[+ position], Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+            ListView listView = (ListView) findViewById(R.id.mobile_list);
+            listView.setAdapter(adapter);
+
+            BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+            BottomNavigationController.setupBottomNavBar(bottomBar,STATE_FOR_BOTTOM_MENY, this);
+
+        }
 }
