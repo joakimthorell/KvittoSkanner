e97a64d27031bc2f1c922e127edb27983f78eb70
lasanjin <slavnic@student.chalmers.se>
2017-05-10 17:46:56 +0200

    DataHolder is back

---
+++ foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/model/DataHolder.java
@@ -0,0 +1,40 @@
+package corp.skaj.foretagskvitton.model;
+
+import android.app.Application;
+
+import java.util.List;
+
+import corp.skaj.foretagskvitton.model.Company;
+import corp.skaj.foretagskvitton.model.Employee;
+import corp.skaj.foretagskvitton.model.User;
+
+public class DataHolder extends Application {
+    private boolean ONCE = true;
+    private List<String> strings;
+    private User user;
+
+    public void setStrings(List<String> strings) {
+        this.strings = strings;
+    }
+
+    public void setUser(User user) {
+        if (ONCE) {
+            this.user = user;
+            ONCE = false;
+        }
+    }
+
+    public User getUser() {
+        if (user == null) {
+            user = new User("MASTER USER");
+            Company company = new Company("DEFAULT COMPANY");
+            user.addCompany(company);
+            user.getCompany(company).addEmployee(new Employee(user.getName()));
+        }
+        return user;
+    }
+
+    public List<String> getStrings() {
+        return strings;
+    }
+}
