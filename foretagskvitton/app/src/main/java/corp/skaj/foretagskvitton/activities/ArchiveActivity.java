b398df054d6c4f99dc8ad137dc813402564076e8
mattssonj <joamatt@student.chalmers.se>
2017-05-10 12:59:10 +0200

    made small changes, error not fixed yet

--- foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/activities/ArchiveActivity.java
+++ foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/activities/ArchiveActivity.java
@@ -1,51 +1,41 @@
 package corp.skaj.foretagskvitton.activities;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Toast;
-
-import com.roughike.bottombar.BottomBar;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import corp.skaj.foretagskvitton.R;
 
-import corp.skaj.foretagskvitton.controllers.BottomNavigationController;
 import corp.skaj.foretagskvitton.controllers.DataHolder;
 import corp.skaj.foretagskvitton.controllers.ReceiptAdapter;
 import corp.skaj.foretagskvitton.model.Receipt;
 import corp.skaj.foretagskvitton.model.Company;
 import corp.skaj.foretagskvitton.model.Employee;
 import corp.skaj.foretagskvitton.model.Purchase;
-import corp.skaj.foretagskvitton.controllers.BottomNavigationController;
 
 public class ArchiveActivity extends AbstractActivity {
-    private List<Receipt> receiptsList = new ArrayList<>();
+    public static final String STATE_FOR_BOTTOM_MENU = "ArchiveActivity";
+    private List<Receipt> receiptsList;
     private RecyclerView recyclerView;
     private ReceiptAdapter mAdapter;
-    protected Context mContext;
-    public static final String STATE_FOR_BOTTOM_MENU = "ArchiveActivity";
 
-    DataHolder dataHolder = (DataHolder)mContext.getApplicationContext();
-    List<Company> companiesList = dataHolder.getUser().getCompanies();
+    DataHolder dataHolder;
+    List<Company> companiesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.archive_activity_main);
+        setContentView(R.layout.activity_archive);
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
-        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
-        BottomNavigationController.setupBottomNavBar(bottomBar, STATE_FOR_BOTTOM_MENU, this);
+        receiptsList = new ArrayList<>();
+        dataHolder = (DataHolder) getApplicationContext();
+        companiesList = dataHolder.getUser().getCompanies();
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
@@ -55,7 +45,6 @@ public class ArchiveActivity extends AbstractActivity {
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
 
-        initBottomBar(STATE_FOR_BOTTOM_MENU, this);
         prepareReceiptData();
     }
 
