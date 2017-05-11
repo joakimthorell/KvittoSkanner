daaa4baf6e1db27aae39207fd8ac13cd60f7192b
KevinBrunstrom <Kevinbr@student.chalmers.se>
2017-05-11 10:02:35 +0200

    Tjoho

--- foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/activities/ArchiveActivity.java
+++ foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/activities/ArchiveActivity.java
@@ -57,23 +57,16 @@ public class ArchiveActivity extends AbstractActivity {
         }
 
     }
-/*
-    private void prepareReceiptData() {
-        for (int i = 0; i < purchases.size(); i++) {
-            List<Employee> employees = dataHolder.getUser();
-            for (int j = 0; j < employees.size(); i++) {
-                List<Purchase> purchases = employees.get(j).getPurchases();
-                for (int k = 0; k < purchases.size(); k++) {
-                    purchases.get(k).getReceipt().getDate();
-                }
-            }
+//.get(i).getEmployees().get(i).getPurchases().get(i);
+    private void collectAllPurchases(){
+            List<Company> company = dataHolder.getUser().getCompanies();
+        for(int i = 0; i < company.size(); i++){
+            List<Employee> emps = dataHolder.getUser().getCompanies().get(i).getEmployees();
         }
-
-        mAdapter.notifyDataSetChanged();
     }
-    */
 
     private void testAddPurchases() {
+
         Product product = new Product("Aladob", "Mat", 50.0, 12.0);
         List<Product> products = new ArrayList<>();
           products.add(product);
@@ -87,48 +80,7 @@ public class ArchiveActivity extends AbstractActivity {
 
         PrivatePurchase pur = new PrivatePurchase(receipt);
         purchases.add(pur);
-      /*purchases.add(pur);
-        purchases.add(pur);
-        purchases.add(pur);
-        purchases.add(pur);
-*/
-        Company com = new Company("Kevins onda foretag AB");
-   /*     Company com1 = new Company("Kevins onda foretag A");
-        Company com2 = new Company("Kevins onda foretag ");
-        Company com3 = new Company("Kevins onda foreta");
-        Company com4 = new Company("Kevins onda foret");
-        Company com5 = new Company("Kevins onda for");
-        Company com6 = new Company("Kevins onda fo");
-        Company com7 = new Company("Kevins onda f");
-        */
-
-       /* dataHolder.getUser().addCompany(com2);
-        dataHolder.getUser().addCompany(com3);
-        dataHolder.getUser().addCompany(com4);
-        dataHolder.getUser().addCompany(com5);
-        dataHolder.getUser().addCompany(com6);
-        dataHolder.getUser().addCompany(com7);
-*/
-        Employee emp = new Employee("Kevin1");
-       /* Employee emp1 = new Employee("Kevin2");
-        Employee emp2 = new Employee("Kevin3");
-        Employee emp3 = new Employee("Kevin4");
-        Employee emp4 = new Employee("Kevin5");
-        Employee emp5 = new Employee("Kevin6");
-        Employee emp6 = new Employee("Kevin7");
-        Employee emp7 = new Employee("Kevin8");
-        */
-       emp.addPurchase(pur);
-        com.addEmployee(emp);
-       /* dataHolder.getUser().getCompanies().get(1).addEmployee(emp1);
-        dataHolder.getUser().getCompanies().get(2).addEmployee(emp2);
-        dataHolder.getUser().getCompanies().get(3).addEmployee(emp3);
-        dataHolder.getUser().getCompanies().get(4).addEmployee(emp4);
-        dataHolder.getUser().getCompanies().get(5).addEmployee(emp5);
-        dataHolder.getUser().getCompanies().get(6).addEmployee(emp6);
-        dataHolder.getUser().getCompanies().get(7).addEmployee(emp7);
-        */
-        dataHolder.getUser().addCompany(com);
+        dataHolder.getUser().getCompanies().get(0).getEmployees().get(0).addPurchase(pur);
         mAdapter.notifyDataSetChanged();
     }
 }
