51b6fe8c996eb54ba37e3a12919911eb91f6894c
annekeller <annekeller93@gmail.com>
2017-05-15 14:16:15 +0200

    still working on the company activity

--- foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/activities/CompanyActivity.java
+++ foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/activities/CompanyActivity.java
@@ -4,13 +4,15 @@ import android.support.v7.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
 import android.widget.TextView;
 
 import corp.skaj.foretagskvitton.R;
+import corp.skaj.foretagskvitton.controllers.CompanyListController;
 
 import static corp.skaj.foretagskvitton.controllers.CompanyListController.COMPANY_KEY;
 
-public class CompanyActivity extends AppCompatActivity {
+public class CompanyActivity extends AbstractActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,12 @@ public class CompanyActivity extends AppCompatActivity {
         Button button = (Button) findViewById(R.id.buttonForCompany);
         listViewController.initButtonListener(button, this);*/
 
+        CompanyListController companyListController = new CompanyListController();
+
+        Button button = (Button) findViewById(R.id.edit);
+        companyListController.editButtonListener(button);
+
+        
         Toolbar toolbar = (Toolbar) findViewById(R.id.company_action_bar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
--- foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/controllers/CompanyListController.java
+++ foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/controllers/CompanyListController.java
@@ -1,9 +1,12 @@
 package corp.skaj.foretagskvitton.controllers;
 
+import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 
 import corp.skaj.foretagskvitton.activities.CompanyActivity;
@@ -26,16 +29,58 @@ public class CompanyListController <T> {
             }
         });
     }
+
+    public void editButtonListener(Button button) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            //När vi klickar på denna knappen vill vi öppna upp för redigering
+                //Byta namn på knappen till spara
+            }
+        });
+
+    }
+
+    public void createNewEmployeeListener (Button button) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Här vill vi lägga till en ny anställd i listan av anställda
+            }
+        });
+
+    }
+
+    public void createNewCardListener (Button button) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Här vill vi lägga till ett nytt kort i listan av kort
+            }
+        });
+
+    }
+
+    public void createNewCommentListener (Button button) {
+        button.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+             //Här vill vi lägga till en ny kommentar i listan av kommentarer
+         }
+     });
+    }
 }
 
     /*public void initButtonListener (Button button, final Activity activity) {
 
         public void initButtonListener (Button button,final Activity activity){
             button.setOnClickListener(new View.OnClickListener() {
+                @Override
                 public void onClick(View v) {
                     activity.finish();
                 }
-            });
+            }
         }
     }
-}*/
+}
+*/
