ee44fff63a16de0ad48085576f6d3db76dd55e87
lasanjin <slavnic@student.chalmers.se>
2017-05-18 13:20:21 +0200

    moved wizard 'what seems to be a model' to view

---
+++ foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/view/TotalSumPage.java
@@ -0,0 +1,11 @@
+package corp.skaj.foretagskvitton.view;
+
+import com.tech.freak.wizardpager.model.ModelCallbacks;
+import com.tech.freak.wizardpager.model.TextPage;
+
+public class TotalSumPage extends TextPage {
+
+    public TotalSumPage(ModelCallbacks callbacks, String title) {
+        super(callbacks, title);
+    }
+}
