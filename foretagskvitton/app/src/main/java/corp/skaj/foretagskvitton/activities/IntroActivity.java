fb74bb68618ff3e59ce5cd13a96b244ac7177b91
annekeller <annekeller93@gmail.com>
2017-05-23 12:34:11 +0200

    baby change

--- foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/activities/IntroActivity.java
+++ foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/activities/IntroActivity.java
@@ -1,7 +1,5 @@
 package corp.skaj.foretagskvitton.activities;
 
-import android.app.Fragment;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
@@ -16,23 +14,23 @@ public class IntroActivity extends AppIntro {
         super.onCreate(savedInstanceState);
 
         addSlide(AppIntroFragment.newInstance("Välkommen till KvittoSkanner!",
-                "Denna applikation gör att du på ett enkelt sätt kan bokföra dina kvitton med hjälp av endast några få klick",
+                "Med denna applikation kan du på ett enkelt sätt kan bokföra dina kvitton med hjälp av endast några få klick",
                 R.mipmap.ic_launcher_round, getColor(R.color.colorAccent)));
         addSlide(AppIntroFragment.newInstance("Arkiv",
-                "Under arkiv kan du hantera alla dina kvitton. Du sortera dem utefter pris och datum. " +
+                "Under arkiv kan du hantera alla dina kvitton. Du sortera dem efter pris och datum. " +
                         "Du har möjlighet att lägga till ett nytt kvitto, redigera ett befintligt eller radera ett kvitto helt." ,
                 R.drawable.ic_image , getColor(R.color.colorPrimary)));
         addSlide(AppIntroFragment.newInstance("Företag",
                 "Under företag kan du hantera alla dina företag. Du kan lägga till ett nytt företag, redigera ett befintligt eller radera ett företag helt." +
-                        "Du måste alltid ha minst ett företag.",
+                        " Du måste alltid ha minst ett företag.",
                 R.drawable.ic_image, getColor(R.color.colorAccent)));
         addSlide(AppIntroFragment.newInstance("Grossister",
                 "Under grossister kan du hantera alla dina grossister. Du kan lägga till en ny grossist, redigera en befintlig eller radera en grossist helt.",
                 R.drawable.ic_image, getColor(R.color.colorPrimary)));
-        addSlide(AppIntroFragment.newInstance("Lägg till", "För att lägga till något nyttjar du den runda lägg till knappen som finns på samtliga tre sidor",
-                R.drawable.ic_image, getColor(R.color.colorAccent)));
+        addSlide(AppIntroFragment.newInstance("Lägg till", "För att lägga till något nytt nyttjar du den runda lägg-till knappen som finns på samtliga tre sidor",
+                R.drawable.introduction_button, getColor(R.color.colorAccent)));
         addSlide(AppIntroFragment.newInstance("Lycka till med bokföringen!", "",
-                R.drawable.ic_image, getColor(R.color.colorPrimary)));
+                R.mipmap.ic_launcher, getColor(R.color.colorPrimary)));
 
     }
 
