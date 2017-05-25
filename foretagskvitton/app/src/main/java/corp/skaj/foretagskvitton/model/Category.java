abd8b8d487bffdc6808e4ca50f268f8f115810d2
annekeller <annekeller93@gmail.com>
2017-05-25 16:16:37 +0200

    changes to category

--- foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/model/Category.java
+++ foretagskvitton/app/src/main/java/corp/skaj/foretagskvitton/model/Category.java
@@ -10,13 +10,11 @@ import java.util.List;
 public enum Category {
     BENSIN("#E45664"), // red
     HOTELL("#FFE0AB"), // light yellow
-    KONTORSMATERIAL("#FFCCE5"), // baby pink
     MAT("#B7E5B7"), // green
-    PORTO("#C0D6E4"), // light blue
-    REPRESENTATION("#FFCC6F"), // orange/brown
+    REPRESENTATION("#C0D6E4"), // light blue
     RESOR("#99D5CF"), // turqoise
-    TRANSPORT("#4B86B4"), // darker blue
-    OTHER("#5B005B"); // purple
+    TRANSPORT("#FFCCE5"), // baby pink
+    ÖVRIGT("#5B005B"); // purple
 
     private final String color;
 
